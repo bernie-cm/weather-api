0f4f83d5bcd6e081a05f1996b95b999c8d4fa02c
bernie-cm <bernie.camejo@icloud.com>
2025-02-03 21:53:32 +1000

    Feat: Finish setting up API call

--- src/app.py
+++ src/app.py
@@ -20,36 +20,24 @@ def get_weather_data(location, connection):
         "q": location,
         "aqi": "yes"        # Provides Air Quality Index
     }
+    api_url = "http://api.weatherapi.com/v1/current.json"
+    response = requests.get(api_url, params=payload)
 
-    response = requests.get
-
-
-
-    # Create your key and replace mine with it
-    payload = {'Key': 'fed028b417bf48408c552501221407', 'q': location, 'aqi': 'yes'}
-    r = requests.get("http://api.weatherapi.com/v1/current.json", params=payload)
-
-    # Get the json from the request's result
-    r_string = r.json()
+    # Extract the JSON from the response variable
+    response_string = response.json()
 
     # Take only the current part of the JSON
-    current = r_string['current']
+    current = response_string['current']
 
     # Fix time format from YYYY-MM-DD hh:mm:ss to -> YYYY-MM-DDThh:mm:ssZ
-    # create datetime object from string
     origin_time = dt.datetime.strptime(current['last_updated'],'%Y-%m-%d %H:%M') 
     
     #turn datetime into formated string for tdengine
-    # Keep in mind this will create timestamps in zulu time. The api only sends you local time.
-    # We should fix this, but I can't be bothered right now
     current['last_updated'] = origin_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    #print(current)  
-    # write the weather data to tdengine
-    write_weather(location,current,conn)    
+    # Call write_weather to write data to TDEngine
+    write_weather(location, current, connection)
 
-    # we don't need this right now, but I kept it
-    return current
 
 
 
